1cff1e964cc616a6e432f46e8b94ad9241400509
Santi Liwsakphaiboon <oofpokemon@gmail.com>
2021-04-05 11:44:53 +0700

    add for loop to mock data add event listenner by using event delegation

--- renderer.js
+++ renderer.js
@@ -2,34 +2,16 @@ const { ipcRenderer } = require("electron");
 const { BrowserWindow } = require("electron").remote;
 const path = require("path");
 
-const asyncMsgBtn = document.getElementById("send-message");
+const questionContainer = document.getElementById('question-container');
 
-asyncMsgBtn.addEventListener('click', () => {
-  const answer = 'test-answer'; //mock answer;
-  ipcRenderer.send('open-answer', answer);
-});
-// asyncMsgBtn.addEventListener("click", () => {
-//   ipcRenderer.send("asynchronous-message", "ping");
-//   const modalPath = path.join("file://", __dirname, "/modal.html");
-//   let win = new BrowserWindow({
-//      width: 400, 
-//      height: 320,
-//      webPreferences: {
-//        nodeIntegration: true,
-//        contextIsolation: false
-//      }
-//     });
-
-//   win.on("close", () => {
-//     win = null;
-//   });
-//   win.loadURL(modalPath);
-//   win.show();
-//   win.webContents.send("answer-data", "answer is A");
-//   win.webContents.openDevTools();
-// });
-
-// ipcRenderer.on("asynchronous-reply", (event, arg) => {
-//   const message = `Asynchronous message reply: ${arg}`;
-//   document.getElementById("reply-message").innerHTML = message;
-// });
+for(let i =0; i<10; i++) {
+  const btn = document.createElement('button');
+  btn.id = i;
+  btn.textContent = i;
+  questionContainer.appendChild(btn);
+}
+questionContainer.addEventListener('click', (e) => {
+  if(e.target && e.target.nodeName === "BUTTON"){
+    ipcRenderer.send('open-answer', e.target.id);
+  }
+})
