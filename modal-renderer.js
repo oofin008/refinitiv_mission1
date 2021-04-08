9a888523991d7721dbe957df5b001cbaa02aabfb
Santi Liwsakphaiboon <oofpokemon@gmail.com>
2021-04-08 15:19:20 +0700

    finish fetch answer data

--- modal-renderer.js
+++ modal-renderer.js
@@ -1,6 +1,18 @@
 const { ipcRenderer } = require('electron');
 
+async function getAnswer(id) {
+  let data = {};
+  const result = await window.fetch(`http://localhost:3000/getanswer/${id}`);
+  data = await result.json();
+  return data.data;
+}
+
 ipcRenderer.on('show-answer', (event, arg) => {
-  const message = `Answer data: ${arg}`
-  document.getElementById('modal-message').innerHTML = message;
+  getAnswer(arg).then(result => {
+    const message = `Answer id: ${result.id} data: ${result.answer}`
+    document.getElementById('modal-message').innerHTML = message;
+  }).catch(() => {
+    const message = `Error occured, something went wrong`;
+    document.getElementById('modal-message').innerHTML = message;
+  })
 })
--- renderer.js
+++ renderer.js
@@ -2,7 +2,7 @@ const { ipcRenderer } = require("electron");
 
 const questionContainer = document.getElementById("question-container");
 
-async function preloadQuestion () {
+async function loadQuestion () {
   let data = [];
   const result = await window.fetch('http://localhost:3000/getquestion');
   data = await result.json();
@@ -10,17 +10,18 @@ async function preloadQuestion () {
 
 }
 
-preloadQuestion().then(result => {
+loadQuestion().then(result => {
   for(let i =0; i < result.length; i++) {
     const btn = document.createElement("button");
     btn.id = result[i].id;
     btn.innerText = result[i].question;
+    btn.setAttribute("answerid", result[i].answerId);
     questionContainer.appendChild(btn);
   }
 })
 
 questionContainer.addEventListener("click", (e) => {
   if (e.target && e.target.nodeName === "BUTTON") {
-    ipcRenderer.send("open-answer", e.target.id);
+    ipcRenderer.send("open-answer", e.target.getAttribute('answerid'));
   }
 });
