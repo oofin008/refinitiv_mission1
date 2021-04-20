2bcdfe8586a0fede67dc8e15a5ed4a0190664234
Santi Liwsakphaiboon <oofpokemon@gmail.com>
2021-04-20 15:35:38 +0700

    refactor: use IIFE to avoid global var

--- main.ts
+++ main.ts
@@ -4,105 +4,109 @@ import * as path from 'path';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
-let mainWindow:BrowserWindow | undefined = undefined;
-let childWindow:BrowserWindow | undefined = undefined;
-let isAppQuitting:boolean = false;
 
-const initialize = ():void => {
-  const createWindow = ():void => {
-    // Create the browser window.
-    mainWindow = new BrowserWindow({
-      width: 800,
-      height: 600,
+//IIFE immidiately-invoked function expression to avoid global var
+(()=> {
+  let mainWindow:BrowserWindow | undefined = undefined;
+  let childWindow:BrowserWindow | undefined = undefined;
+  let isAppQuitting:boolean = false;
+  
+  const initialize = ():void => {
+    const createWindow = ():void => {
+      // Create the browser window.
+      mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+          nodeIntegration: true,
+          contextIsolation: false,
+          enableRemoteModule: true,
+          preload: path.join(__dirname, "preload.js"),
+        },
+      });
+      mainWindow.loadFile("index.html");
+      mainWindow.webContents.openDevTools();
+  
+      createChildwindow(mainWindow);
+      childWindow!.webContents.openDevTools();
+  
+      // Attach IPC event
+      ipcMain.on("open-answer", (event: Electron.IpcMainEvent, arg: any):void => {
+        if (childWindow == null) {
+          console.log("child");
+          createChildwindow(mainWindow);
+        }
+        childWindow!.show();
+        childWindow!.webContents.send("show-answer", arg);
+      });
+  
+      ipcMain.on("no-action", (event: Electron.IpcMainEvent, arg: any):void => {
+        if ( childWindow != null){
+          console.log('Timeout childWindow');
+          childWindow.hide();
+        }
+      });
+  
+      mainWindow.on('close', ():void => {
+        console.log('mainWindow closing...');
+        isAppQuitting = true;
+      });
+      mainWindow.on("closed", ():void => {
+        console.log('mainWindow closed...');
+        mainWindow = undefined;
+      });
+  
+      childWindow!.on('close', (event:Electron.Event):void => {
+        if(!isAppQuitting){
+          console.log('childWindow hiding...');
+          event.preventDefault();
+          childWindow!.hide();
+        } else {
+          console.log('childWindow closing...');
+        }
+      });
+      childWindow!.on('closed', ():void => {
+        console.log('childWindow closed...');
+        childWindow = undefined;
+      });
+    };
+  
+    app.whenReady().then(():void => {
+      createWindow();
+      console.log("== App ready ==");
+      app.on("activate", ():void => {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+      });
+    });
+  
+    app.on("window-all-closed", ():void => {
+      if (process.platform !== "darwin") app.quit();
+      console.log("== App quit ==");
+    });
+  
+    app.on("before-quit", ():void => {
+      isAppQuitting = true;
+      console.log('quitting...');
+    });
+  };
+  
+  const createChildwindow = (parentWindow:BrowserWindow | undefined):void => {
+    isAppQuitting = false;
+    childWindow = new BrowserWindow({
+      width: 400,
+      height: 320,
+      parent: parentWindow,
       webPreferences: {
         nodeIntegration: true,
         contextIsolation: false,
         enableRemoteModule: true,
-        preload: path.join(__dirname, "preload.js"),
       },
+      show: false,
     });
-    mainWindow.loadFile("index.html");
-    mainWindow.webContents.openDevTools();
-
-    createChildwindow(mainWindow);
-    childWindow!.webContents.openDevTools();
-
-    // Attach IPC event
-    ipcMain.on("open-answer", (event: Electron.IpcMainEvent, arg: any):void => {
-      if (childWindow == null) {
-        console.log("child");
-        createChildwindow(mainWindow);
-      }
-      childWindow!.show();
-      childWindow!.webContents.send("show-answer", arg);
-    });
-
-    ipcMain.on("no-action", (event: Electron.IpcMainEvent, arg: any):void => {
-      if ( childWindow != null){
-        console.log('Timeout childWindow');
-        childWindow.hide();
-      }
-    });
-
-    mainWindow.on('close', ():void => {
-      console.log('mainWindow closing...');
-      isAppQuitting = true;
-    });
-    mainWindow.on("closed", ():void => {
-      console.log('mainWindow closed...');
-      mainWindow = undefined;
-    });
-
-    childWindow!.on('close', (event:Electron.Event):void => {
-      if(!isAppQuitting){
-        console.log('childWindow hiding...');
-        event.preventDefault();
-        childWindow!.hide();
-      } else {
-        console.log('childWindow closing...');
-      }
-    });
-    childWindow!.on('closed', ():void => {
-      console.log('childWindow closed...');
-      childWindow = undefined;
-    });
+    childWindow.loadFile("modal.html");
   };
-
-  app.whenReady().then(():void => {
-    createWindow();
-    console.log("== App ready ==");
-    app.on("activate", ():void => {
-      // On macOS it's common to re-create a window in the app when the
-      // dock icon is clicked and there are no other windows open.
-      if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
-  });
-
-  app.on("window-all-closed", ():void => {
-    if (process.platform !== "darwin") app.quit();
-    console.log("== App quit ==");
-  });
-
-  app.on("before-quit", ():void => {
-    isAppQuitting = true;
-    console.log('quitting...');
-  });
-};
-
-const createChildwindow = (parentWindow:BrowserWindow | undefined):void => {
-  isAppQuitting = false;
-  childWindow = new BrowserWindow({
-    width: 400,
-    height: 320,
-    parent: parentWindow,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-      enableRemoteModule: true,
-    },
-    show: false,
-  });
-  childWindow.loadFile("modal.html");
-};
-
-initialize();
+  
+  initialize();
+})();
